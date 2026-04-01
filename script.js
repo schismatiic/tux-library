216015d6fe8e82bd2ba2a6a682f9e4eb61bbe955
schismatiic <leonardojofre2003@gmail.com>
2026-03-31 21:02:38 -0300

    Default books added

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const myLibrary = [];
-
-function Book(id, name, author) {
+function Book(id, name, author, numberOfPages) {
   this.id = id;
   this.name = name;
   this.author = author;
+  this.numberOfPages = numberOfPages;
 }
-function addBookToLibrary() {}
+// ====================== DEFAULT BOOKS ======================
+const defaultBook1 = new Book(
+  crypto.randomUUID(),
+  "Don Quijote de la Mancha",
+  "Miguel de Cervantes",
+  1380,
+);
+const defaultBook2 = new Book(
+  crypto.randomUUID(),
+  "Crime and Punishement",
+  "Fyodor Dostoevsky",
+  720,
+);
+const defaultBook3 = new Book(
+  crypto.randomUUID(),
+  "1984",
+  "George Orwell",
+  336,
+);
+// ===========================================================
+// function addBookToLibrary() {}
+
+console.log(defaultBook1);
+console.log(defaultBook2);
+console.log(defaultBook3);
