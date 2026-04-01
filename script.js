7877497ec9cc2ef841c6edf159fa1ee58c584bab
schismatiic <leonardojofre2003@gmail.com>
2026-04-01 00:37:32 -0300

    Ahora se renderizan los libros

--- script.js
+++ script.js
@@ -24,9 +24,40 @@ const defaultBook3 = new Book(
   "George Orwell",
   336,
 );
+myLibrary[0] = defaultBook1;
+myLibrary[1] = defaultBook2;
+myLibrary[2] = defaultBook3;
 // ===========================================================
-// function addBookToLibrary() {}
 
-console.log(defaultBook1);
-console.log(defaultBook2);
-console.log(defaultBook3);
+// function addBookToLibrary() {}
+const tux = document.getElementById("tux");
+const book__box = document.getElementById("book-container");
+
+// =========================== TUX ===========================
+tux.addEventListener("click", () => {
+  tux.src = "https://media.tenor.com/S61VCO73mOAAAAAj/linux-tux.gif";
+});
+// ===========================================================
+const nuevo = document.createElement("p");
+
+myLibrary.forEach((book, index) => {
+  const bookItem = document.createElement("div");
+
+  const name = document.createElement("h3");
+  const author = document.createElement("p");
+  const numberOfPages = document.createElement("p");
+
+  bookItem.className = "book";
+  name.className = "book__name";
+  author.className = "book__author";
+  numberOfPages.className = "book__pages";
+
+  name.textContent = `${book.name}`;
+  author.textContent = `Author: ${book.author}`;
+  numberOfPages.textContent = `Number of pages: ${book.numberOfPages}`;
+
+  bookItem.appendChild(name);
+  bookItem.appendChild(author);
+  bookItem.appendChild(numberOfPages);
+  book__box.appendChild(bookItem);
+});
