44c558321a2ad8d1ad1a82600c49e4e4d15e727f
schismatiic <leonardojofre2003@gmail.com>
2026-04-01 23:56:49 -0300

    Función para agregar libros mediante código

--- script.js
+++ script.js
@@ -5,31 +5,22 @@ function Book(id, name, author, numberOfPages) {
   this.author = author;
   this.numberOfPages = numberOfPages;
 }
+const addBookToLibrary = (name, author, numberOfPages) => {
+  const newBook = new Book(crypto.randomUUID(), name, author, numberOfPages);
+  // const newLibrary = [...myLibrary, newBook];
+  myLibrary.push(newBook);
+  return myLibrary;
+};
 // ====================== DEFAULT BOOKS ======================
-const defaultBook1 = new Book(
-  crypto.randomUUID(),
+const donQuijote = addBookToLibrary(
   "Don Quijote de la Mancha",
   "Miguel de Cervantes",
   1380,
 );
-const defaultBook2 = new Book(
-  crypto.randomUUID(),
-  "Crime and Punishement",
-  "Fyodor Dostoevsky",
-  720,
-);
-const defaultBook3 = new Book(
-  crypto.randomUUID(),
-  "1984",
-  "George Orwell",
-  336,
-);
-myLibrary[0] = defaultBook1;
-myLibrary[1] = defaultBook2;
-myLibrary[2] = defaultBook3;
+const crimeAndPunishment = addBookToLibrary("Punishement", "Dostoevsky", 720);
+const orwell1984 = addBookToLibrary("1984", "Orwell", 336);
 // ===========================================================
 
-// function addBookToLibrary() {}
 const tux = document.getElementById("tux");
 const book__box = document.getElementById("book-container");
 
@@ -40,24 +31,29 @@ tux.addEventListener("click", () => {
 // ===========================================================
 const nuevo = document.createElement("p");
 
-myLibrary.forEach((book, index) => {
-  const bookItem = document.createElement("div");
+const renderizeBooks = () => {
+  myLibrary.forEach((book) => {
+    const bookItem = document.createElement("div");
 
-  const name = document.createElement("h3");
-  const author = document.createElement("p");
-  const numberOfPages = document.createElement("p");
+    const name = document.createElement("h3");
+    const author = document.createElement("p");
+    const numberOfPages = document.createElement("p");
 
-  bookItem.className = "book";
-  name.className = "book__name";
-  author.className = "book__author";
-  numberOfPages.className = "book__pages";
+    bookItem.className = "book";
+    name.className = "book__name";
+    author.className = "book__author";
+    numberOfPages.className = "book__pages";
 
-  name.textContent = `${book.name}`;
-  author.textContent = `Author: ${book.author}`;
-  numberOfPages.textContent = `Number of pages: ${book.numberOfPages}`;
+    name.textContent = `➤ ${book.name}`;
+    author.textContent = `Author: ${book.author}`;
+    numberOfPages.textContent = `Number of pages: ${book.numberOfPages}`;
 
-  bookItem.appendChild(name);
-  bookItem.appendChild(author);
-  bookItem.appendChild(numberOfPages);
-  book__box.appendChild(bookItem);
-});
+    bookItem.appendChild(name);
+    bookItem.appendChild(author);
+    bookItem.appendChild(numberOfPages);
+    book__box.appendChild(bookItem);
+  });
+};
+renderizeBooks();
+console.log(hola);
+console.log(hola123);
