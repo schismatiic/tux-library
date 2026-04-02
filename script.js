672777d960b7884ee029b1d5f946bd0738c9b58c
schismatiic <leonardojofre2003@gmail.com>
2026-04-02 00:50:03 -0300

    Dialog al intentar añadir un libro

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const myLibrary = [];
+// ======================= CONSTRUCTOR =======================
 function Book(id, name, author, numberOfPages) {
   this.id = id;
   this.name = name;
   this.author = author;
   this.numberOfPages = numberOfPages;
 }
+
+// =========================== DOM ===========================
+const tux = document.getElementById("tux");
+const book__box = document.getElementById("book-container");
+const addButton = document.getElementById("new-book");
+
+// ======================== ADD BOOKS ========================
 const addBookToLibrary = (name, author, numberOfPages) => {
   const newBook = new Book(crypto.randomUUID(), name, author, numberOfPages);
-  // const newLibrary = [...myLibrary, newBook];
   myLibrary.push(newBook);
   return myLibrary;
 };
+
 // ====================== DEFAULT BOOKS ======================
 const donQuijote = addBookToLibrary(
   "Don Quijote de la Mancha",
@@ -19,18 +27,13 @@ const donQuijote = addBookToLibrary(
 );
 const crimeAndPunishment = addBookToLibrary("Punishement", "Dostoevsky", 720);
 const orwell1984 = addBookToLibrary("1984", "Orwell", 336);
-// ===========================================================
-
-const tux = document.getElementById("tux");
-const book__box = document.getElementById("book-container");
 
 // =========================== TUX ===========================
 tux.addEventListener("click", () => {
   tux.src = "https://media.tenor.com/S61VCO73mOAAAAAj/linux-tux.gif";
 });
-// ===========================================================
-const nuevo = document.createElement("p");
 
+// ===================== RENDERIZE BOOKS =====================
 const renderizeBooks = () => {
   myLibrary.forEach((book) => {
     const bookItem = document.createElement("div");
@@ -55,5 +58,3 @@ const renderizeBooks = () => {
   });
 };
 renderizeBooks();
-console.log(hola);
-console.log(hola123);
